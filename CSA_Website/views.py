2129cb2e225a90c7b99e485424fb257115af6e56
SGmagicsam <38333794+SGmagicsam@users.noreply.github.com>
2019-10-19 21:53:24 -0600

    Houston we have images

--- CSA_Website/views.py
+++ CSA_Website/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts 		   import render
+
+
+
 
 # Create your views here.
 def home(request ):
+
 
 
 	return render(request, "base/base_plate.html")
